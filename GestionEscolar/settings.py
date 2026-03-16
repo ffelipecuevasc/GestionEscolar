5aafc6872f1962398b0ebc4846e1f800424c07f1
Felipe Cuevas <ffelipecuevasc@gmail.com>
2026-03-16 18:21:07 -0300

    Finalización - Proyecto terminado con mejoras necesarias para desplegarlo en AlwaysData.

--- GestionEscolar/settings.py
+++ GestionEscolar/settings.py
@@ -125,5 +125,6 @@ LOGOUT_REDIRECT_URL = '/login/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
